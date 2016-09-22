8d7242b073146019f6a50acfa4773494712466bf
Miguel Amores Tamarit <miamta@floridauniversitaria.es>
2016-09-22 10:26:34 +0200

    primer commit

---
+++ paco.php
@@ -0,0 +1,13 @@
+//fichero index.php
+//creado por Paco Gomez
+<html>
+  <head>
+      <title>ACTIVIDAD 03</title>
+      <?php
+        echo "Aquí poner vuestro nombre;
+        echo "Aquí poner vuestro primer apellido";
+        print "Aquí poner vuestro segundo apellido";
+      ?>
+  </head>
+  <body>
+</html>
